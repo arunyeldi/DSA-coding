2aca0500040424e998a61a1b1174e68ea7fb18c5
arunyeldi <123795369+arunyeldi@users.noreply.github.com>
2023-12-21 22:42:11 +0530

    Time: 5 ms (40.48%), Space: 9.1 MB (15.73%) - LeetHub

---
+++ 0804-unique-morse-code-words/0804-unique-morse-code-words.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+        vector<string> v={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+
+        set<string> s;
+        for(auto word : words){
+            string morseCode;
+            for(char c : word){
+                morseCode += v[c - 'a'];
+            }
+            s.insert(morseCode);
+        }
+
+        return s.size();   
+    }
+};
